a03e3f3eeb5deec6a1bc146bac7620159c72b3da
antoinekhouri <antoine.khouri@mail.mcgill.ca>
2020-02-09 22:58:13 -0500

    Add accuracy model

--- p1/naive_bayes/naive_bayes.py
+++ p1/naive_bayes/naive_bayes.py
@@ -96,33 +96,56 @@ class naive_bayes:
 			self.likelihood_type.append(likelihood_type[i])
 
 	#Get the mean and standard deviation of a set of data for each output
-	def calc_mean_and_std(self, data):
-
+	def calc_mean_and_std(self, data, y_data , behaviour):
 		zero_data = []
 		one_data = []
+		if behaviour == 0:
 		#For each instance, check if the output is 0 or 1 and separate the data
 		#Appropriately
-		for i in range(len(data)):
-			if self.y[i][0]==self.output_values[0]:
-				if "hepatitis" in filename:
-					data[i] = float(data[i])
-				zero_data.append(data[i])
-			elif self.y[i][0] == self.output_values[1]:
-				if "hepatitis" in filename:
-					data[i] = float(data[i])
-				one_data.append(data[i])
-		if isinstance(zero_data[0], int) or isinstance(zero_data[0], float):
-			zero_mu = np.mean(zero_data) #Mean value of predictor for 0-outputs
-			zero_std = np.std(zero_data) #Standard deviation value of predictor for 0-outputs
-			one_mu = np.mean(one_data) #Mean value of predictor for 1-outputs
-			one_std = np.std(one_data) #Standard deviation value of predictor for 1-outputs
-		else:
-			zero_mu = None
-			zero_std = None
-			one_mu = None
-			one_std = None
-		zero_counts = len(zero_data) #Count of amount of times predictor's value appears for 0-outputs
-		one_counts = len(one_data) #Count of amount of times predictor's value appears for 1-outputs
+			for i in range(len(data)):
+				if self.y[i][0]==self.output_values[0]:
+					if "hepatitis" in filename:
+						data[i] = float(data[i])
+					zero_data.append(data[i])
+				elif self.y[i][0] == self.output_values[1]:
+					if "hepatitis" in filename:
+						data[i] = float(data[i])
+					one_data.append(data[i])
+			if isinstance(zero_data[0], int) or isinstance(zero_data[0], float):
+				zero_mu = np.mean(zero_data) #Mean value of predictor for 0-outputs
+				zero_std = np.std(zero_data) #Standard deviation value of predictor for 0-outputs
+				one_mu = np.mean(one_data) #Mean value of predictor for 1-outputs
+				one_std = np.std(one_data) #Standard deviation value of predictor for 1-outputs
+			else:
+				zero_mu = None
+				zero_std = None
+				one_mu = None
+				one_std = None
+			zero_counts = len(zero_data) #Count of amount of times predictor's value appears for 0-outputs
+			one_counts = len(one_data) #Count of amount of times predictor's value appears for 1-outputs
+		elif behaviour==1:
+			# print(len(data))
+			for i in range(len(data)):
+				if y_data[i]==self.output_values[0]:
+					if "hepatitis" in filename:
+						data[i] = float(data[i])
+					zero_data.append(data[i])
+				elif y_data[i] == self.output_values[1]:
+					if "hepatitis" in filename:
+						data[i] = float(data[i])
+					one_data.append(data[i])
+			if isinstance(zero_data[0], int) or isinstance(zero_data[0], float):
+				zero_mu = np.mean(zero_data) #Mean value of predictor for 0-outputs
+				zero_std = np.std(zero_data) #Standard deviation value of predictor for 0-outputs
+				one_mu = np.mean(one_data) #Mean value of predictor for 1-outputs
+				one_std = np.std(one_data) #Standard deviation value of predictor for 1-outputs
+			else:
+				zero_mu = None
+				zero_std = None
+				one_mu = None
+				one_std = None
+			zero_counts = len(zero_data) #Count of amount of times predictor's value appears for 0-outputs
+			one_counts = len(one_data) #Count of amount of times predictor's value appears for 1-outputs
 		return(zero_mu,zero_std,one_mu, one_std, zero_counts, one_counts)
 
 	#Get the gaussian likelihood  of an attribute for 0 and 1 outputs
@@ -157,7 +180,10 @@ class naive_bayes:
 					tmp = row[feature_index].strip()
 				if tmp == obs:
 					count = count + 1
-		lh = count/total
+		if total == 0:
+			return 0 
+		else:
+			lh = count/total
 		return lh
 
 	#Get the multinomial likelihood
@@ -188,7 +214,10 @@ class naive_bayes:
 					tmp = row[feature_index].strip()
 				if tmp == obs:
 					count = count + 1
-		lh = count/total
+		if(total == 0):
+			return 0
+		else:
+			lh = count/total
 		return lh
 
 	def fit(self):
@@ -202,7 +231,7 @@ class naive_bayes:
 				self.one_data.append(self.X[i])
 		# Set up all the means and std devs
 		for column in self.X.T:
-			res = self.calc_mean_and_std(self, column)
+			res = self.calc_mean_and_std(self, column, [], 0)
 			self.zero_mean_vals.append(res[0])
 			self.zero_std_vals.append(res[1])
 			self.one_mean_vals.append(res[2])
@@ -227,7 +256,9 @@ class naive_bayes:
 		# evidence = self.calculate_evidence(self, data_point)
 		#Initialize some variables
 		results = []
+		counter = 0
 		for point in data_points:
+			counter = counter + 1
 			zero_tmp_likelihood = []
 			one_tmp_likelihood = []
 			zero_total_likelihood=1
@@ -263,6 +294,86 @@ class naive_bayes:
 			else: 
 				results.append(self.output_values[1])
 		return results
+	#k-fold cross validation
+	def accuracy(self, k):
+		counter = 0
+		tmp_X = np.c_[np.ones((self.X.shape[0], 1)), self.X]
+		expected_results = []
+		for row in tmp_X:
+			row[0] = counter
+			counter = counter + 1
+		np.random.shuffle(tmp_X)
+		
+		for row in tmp_X:
+			expected_results.append(self.y[int(row[0])][0])
+		tmp_X = np.delete(tmp_X, 0, 1)
+		fold_length = int(self.instances/k)
+		data_folds = [[[]]]
+		for i in range(k):
+			j=0
+			while j<fold_length:
+				for l in range(self.predictor_count):
+					data_folds[i][j].append(tmp_X[j+i*fold_length][l])
+					# print(data_folds)
+				j = j + 1
+					# print("FUCK YOU")
+				if j<fold_length: 	
+					data_folds[i].append([])
+			data_folds.append([[]])
+		#For each instance, check if the output is 0 or 1 and separate the data
+		#Appropriately
+		results = []
+		for i in range(k):
+			#Re-initialize the variables
+			self.zero_data = []
+			self.one_data = []
+			self.zero_count_vals = [] 
+			self.zero_mean_vals = []
+			self.zero_std_vals = [] 
+			self.one_count_vals = []
+			self.one_mean_vals = []
+			self.one_std_vals = []
+			train_data = []
+			test_data = []
+			train_y = []
+			test_y = []
+			for m in range(k):
+				# print("m: ", m)
+				if i == m:
+					test_data = data_folds[i]
+					for l in range(fold_length):
+						test_y.append(expected_results[i*fold_length+m])
+				else:
+					for l in range(fold_length):
+						train_data.append(data_folds[i][l])
+						if expected_results[i*fold_length+m]==self.output_values[0]:
+							self.zero_data.append(data_folds[i][l])
+						elif expected_results[i*fold_length+m] == self.output_values[1]:
+							self.one_data.append(data_folds[i][l])
+						train_y.append(expected_results[i*fold_length+m])
+			self.compute_priors(self, train_y)
+			# print(train_data)
+		# Set up all the means and std devs
+			for column in list(map(list, zip(*train_data))):
+				res = self.calc_mean_and_std(self, column, train_y, 1)
+				self.zero_mean_vals.append(res[0])
+				self.zero_std_vals.append(res[1])
+				self.one_mean_vals.append(res[2])
+				self.one_std_vals.append(res[3])
+				self.zero_count_vals.append(res[4])
+				self.one_count_vals.append(res[5])
+
+			res = self.predict(self, test_data)
+			for a in res:
+				results.append(a)
+		success = 0
+		for i in range(len(results)):	
+			if results[i] == expected_results[i]:
+				success = success + 1
+		acc = success/len(results)
+		print("Accuracy rate: ", acc*100, "%")
+		return acc
+
 
 if __name__ == "__main__":
 
@@ -279,14 +390,17 @@ if __name__ == "__main__":
 			likelihood[i] = 1
 		out = df1.Output.unique()
 		#Initialize our model with our data
-		x.init(x, df1, likelihood, out, df1.shape[0], df1.shape[1]-1, ypos)
-		x.fit(x)
+		x.init(x,df1,likelihood,out,df1.shape[0],df1.shape[1]-1, ypos)
+		x.accuracy(x,5)
+		# x.fit(x)
 		test_X = [[1,0,0.36876,-1,-1,-1,-0.07661,1,1,0.95041,0.74597,-0.38710,-1,-0.79313,-0.09677,1,0.48684,0.46502,0.31755,-0.27461,-0.14343,-0.20188,-0.11976,0.06895,0.03021,0.06639,0.03443,-0.01186,-0.00403,-0.01672,-0.00761,0.00108,0.00015,0.00325], [1,0,1,-0.08183,1,-0.11326,0.99246,-0.29802,1,-0.33075,0.96662,-0.34281,0.85788,-0.47265,0.91904,-0.48170,0.73084,-0.65224,0.68131,-0.63544,0.82450,-0.78316,0.58829,-0.74785,0.67033,-0.96296,0.48757,-0.85669,0.37941,-0.83893,0.24117,-0.88846,0.29221,-0.89621], [1,0,0.01975,0.00705,0.04090,-0.00846,0.02116,0.01128,0.01128,0.04372,0.00282,0.00141,0.01975,-0.03103,-0.01975,0.06065,-0.04090,0.02680,-0.02398,-0.00423,0.04372,-0.02539,0.01834,0,0,-0.01269,0.01834,-0.01128,0.00564,-0.01551,-0.01693,-0.02398,0.00705,0]]
 		#Since the second predictor is always the same for all instances, remove it from each input point
-		for item in test_X:
-			item.pop(1)
-		res = x.predict(x, test_X)
-		print("Predicted outputs: ", res)
+		# for item in test_X:
+		# 	item.pop(1)
+		# # res = x.predict(x, test_X)
+		# # print("Predicted outputs: ", res)
+		# # acc = x.accuracy(x,5)
+		# print("Ionosphere data model accuracy: ", acc*100, "%")
 	# -------------------------/IONOSPHERE SETUP--------------------------
 
 	# -------------------------ADULT SETUP--------------------------------
@@ -310,10 +424,12 @@ if __name__ == "__main__":
 		out = df1.Output.unique()
 		#Initialize our model with our data
 		x.init(x,df1,likelihood,out,df1.shape[0],df1.shape[1]-1, ypos)
-		x.fit(x)
+		# x.fit(x)
+		x.accuracy(x,5)
 		input_vector = [[30, "State-gov", 141297, "Bachelors", 13, "Married-civ-spouse", "Prof-specialty", "Husband", "Asian-Pac-Islander", "Male", 0, 0, 40, "India"],[39, "Private", 367260, "HS-grad", 9, "Divorced", "Exec-managerial", "Not-in-family", "White", "Male", 0, 0, 80, "United-States"]]
-		res = x.predict(x, input_vector)
-		print("Predicted outputs: ", res)
+		# res = x.predict(x, input_vector)
+		# print("Predicted outputs: ", res)
+		# print("Adult data model accuracy: ", acc*100, "%")
 	# ------------------------/ADULT SETUP---------------------------------
 
 	# ------------------------BREAST CANCER SETUP--------------------------
@@ -332,10 +448,13 @@ if __name__ == "__main__":
 		out=df1.Output.unique()
 		# #Initialize our model with our data
 		x.init(x,df1,likelihood,out,df1.shape[0],df1.shape[1]-1, ypos)
-		x.fit(x)
+		x.accuracy(x,5)
+		# x.fit(x)
 		input_vector =[["60-69","ge40","15-19","0-2","no",2,"left","left_low","no"],["40-49","premeno","30-34","0-2","yes",3,"right","right_up","no"]]
-		res = x.predict(x, input_vector)
-		print("Predicted outputs: ", res)
+		# res = x.predict(x, input_vector)
+		# print("Predicted outputs: ", res)
+		# acc = x.accuracy(x,5)
+		# print("Breast-cancer data model accuracy: ", acc*100, "%")
 	# ------------------------/BREAST CANCER SETUP-------------------------
 
 	# ------------------------HEPATITIS SETUP------------------------------
@@ -363,15 +482,19 @@ if __name__ == "__main__":
 		likelihood[18]=0 #Histology: bernoulli
 		out=df1.Output.unique()
 		x.init(x,df1,likelihood,out,df1.shape[0],df1.shape[1]-1, ypos)
-		x.fit(x)
+		x.accuracy(x,5)
+		
+		# x.fit(x)
+		# acc = x.accuracy(x,5)
+		# print("hepatitis data model accuracy: ", acc*100, "%")
 		input_vector = [[51,1,1,1,1,1,2,2,2,2,2,2,2,1.00,78,58,4.6,52,1], [57,1,1,2,1,1,2,2,2,2,1,1,2,4.60,82,55,3.3,30,2]]
-		res = x.predict(x,input_vector)
-		output_res = [None] * len(res)
-		for i in range(len(res)):
-			if res[i] == 1:
-				output_res[i] = "Die"
-			elif res[i]==2:
-				output_res[i] = "Live"
-		print(output_res)
+		# res = x.predict(x,input_vector)
+		# output_res = [None] * len(res)
+		# for i in range(len(res)):
+		# 	if res[i] == 1:
+		# 		output_res[i] = "Die"
+		# 	elif res[i]==2:
+		# 		output_res[i] = "Live"
+		# print(output_res)
 
 
